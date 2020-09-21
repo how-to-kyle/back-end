44cbeceef7633b081b978210834e7490f26a3cb9
Parker williams <parkster246@gmail.com>
2020-09-21 16:35:53 -0500

    testing on postman needs to be done

--- models/posts-models.js
+++ models/posts-models.js
@@ -4,7 +4,30 @@ module.exports = {
 add,
 get,
 getById,
-insert,
 update,
 remove
+}
+async function add(addedObject) {
+    const id = await db('posts').insert(addedObject)
+    return addedObject
+}
+
+async function get(post) {
+    return await db('posts').where(post)
+}
+async function getById(post_id){
+    const id = await db('posts').where({post_id})
+    return id
+}
+
+async function update(changes, id){
+    if (changes){
+        await db('posts').where({id}).update('title', changes)
+    }
+    return "Title has been updated"
+}
+
+async function remove(post_id) {
+    await db('posts').where({id}).del() 
+    return "Post deleted"
 }
--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -1,10 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const Posts = require('../models/posts-models')
 
-router.post()
+router.post("/", (req, res) => {
+    Posts.add(req.body)
+    .then(response => {res.status(201).json(response)})
+    .catch((error)=> {
+        res.status(500).json( {message: error.message})
+    })
+})
 
-router.put()
+router.put("/:id", (req, res) => {
+    const id = Number(req.params.id)
+    Posts.update(id, req.body)
+    .then(response=>{
+        res.status(200).json(response)
+    })
+    .catch((error)=> {
+        res.status(500).json( {message: error.message})
 
-router.delete()
+    })
+})
 
-router.get()
+router.delete("/id", (req, res)=>{
+    const id = Number(req.params.id)
+    Posts.remove(id, req.body)
+    .then(response=> {
+        res.status(200).json(response)
+    })
+    .catch((error)=> {
+        res.status(500).json({message:error.message})
+    })
+})
+
+router.get("/", (req,res) => {
+    Posts.get()
+    .then((posts) => {
+        res.status(200).json(posts)
+    })
+    .catch((error)=> {
+        res.status(500).json({
+            message: "Error retrieving the posts",
+        })
+    })
+})
+router.get("/:id", (req, res)=> {
+    Posts.getById(Number(req.posts.id))
+    res.status(200).json(posts)
+})
+
+
+module.exports = router;
