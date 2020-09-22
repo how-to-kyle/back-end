8f0882140fa5e3274599a7bc7571821157b3c313
Parker williams <parkster246@gmail.com>
2020-09-22 15:37:52 -0500

    added better seed data

--- data/seeds/001-posts.js
+++ data/seeds/001-posts.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
     return knex('posts').insert([   
-      { title: 'testing seed title',  user_post_id: 1 },
-      { title: 'testing seed title2',  user_post_id: 1 },
-      { title: 'testing seed title3',  user_post_id: 2 }
+      { title: 'Save time by using hotkeys',  user_post_id: 1 },
+      { title: 'Automated testing is the best',  user_post_id: 1 },
+      { title: 'Brush your teeth while showering',  user_post_id: 2 }
     ]);
   };
